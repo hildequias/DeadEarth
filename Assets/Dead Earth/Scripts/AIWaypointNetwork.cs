e5b9d8ceb4bb37b5084530360decdb7437b7c778
Hildequias Junior <juniorwelling@yahoo.com.br>
2017-06-30 23:55:42 -0300

    added paths

---
+++ Assets/Dead Earth/Scripts/AIWaypointNetwork.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AIWaypointNetwork : MonoBehaviour {
+
+    public List<Transform> waypoints = new List<Transform>();
+    
+
+}
